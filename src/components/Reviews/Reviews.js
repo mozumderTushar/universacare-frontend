7bc124ed2b76d38e404dedd00fb3aab5ce7ec430
mozumderTushar <itztushar55@gmail.com>
2021-05-30 12:45:34 +0600

    slider added review section for mbl

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Col, Row, Card } from 'react-bootstrap';
 import './Reviews.css'
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
 const Reviews = () => {
 
@@ -10,7 +12,7 @@ const Reviews = () => {
       name: 'Alvin Stanley',
       review: 'Great company to work for! This organization cares for its team of caregivers, office employees, and clients. There is always someone available to pick up the phone when you need help.',
       see: 'SEE ALL REVIEWS →'
-   
+
     },
     {
       id: 2,
@@ -24,27 +26,61 @@ const Reviews = () => {
     }
   ];
 
+  const primaryOptions = { /** slider option */
+    type: 'loop',
+    gap: '1rem',
+    autoplay: true,
+    pauseOnHover: false,
+    resetProgress: false,
+    arrows: 'slider',
+  };
+
   return (
-    <div className="reviews">
-      <Row>
+    <div>
+      <div className="reviews">
+        <Row>
+          <Col md={2} md={{ offset: 1 }}>
+            <h1>5.0</h1>
+            <i class="fas fa-star review__icon review__icon"></i><i class="fas fa-star review__icon"></i><i class="fas fa-star review__icon"></i><i class="fas fa-star review__icon"></i><i class="fas fa-star review__icon"></i>
+            <p className="reviews__verified">2,054 verified customers reviews by <strong>Google</strong>.</p>
+          </Col>
+          {
+            data.map(review => (
+              <Col md={3} key={review.id}>
+                <Card body className="review__card">
+                  <h6>{review.name}</h6>
+                  <i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i>
+                  <p className="review__user">{review.review}</p>
+                </Card>
+                <p className="see__more__review">{review.see}</p>
+              </Col>
+            ))
+          }
+        </Row>
+      </div>
+      <div className="slider__mbl">
         <Col md={2} md={{ offset: 1 }}>
           <h1>5.0</h1>
           <i class="fas fa-star review__icon review__icon"></i><i class="fas fa-star review__icon"></i><i class="fas fa-star review__icon"></i><i class="fas fa-star review__icon"></i><i class="fas fa-star review__icon"></i>
           <p className="reviews__verified">2,054 verified customers reviews by <strong>Google</strong>.</p>
         </Col>
-        {
-          data.map(review => (
-            <Col md={3} key={review.id}>
-              <Card body className="review__card">
-                <h6>{review.name}</h6>
-                <i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i>
-                <p className="review__user">{review.review}</p>
-              </Card>
-              <p className="see__more__review">{review.see}</p>
-            </Col>
-          ))
-        }
-      </Row>
+        <Splide options={primaryOptions}>
+          {/**--------------------------------- -------- Slider --------------------------------*/
+            data.map(review => (
+              <SplideSlide>
+                <Col md={3} key={review.id}>
+                  <Card body className="review__card">
+                    <h6>{review.name}</h6>
+                    <i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i><i class="fas fa-star review__icon__user"></i>
+                    <p className="review__user">{review.review}</p>
+                  </Card>
+                  <p className="see__more__review">{review.see}</p>
+                </Col>
+              </SplideSlide>
+            ))
+          }
+        </Splide>
+      </div>
     </div>
   );
 };
